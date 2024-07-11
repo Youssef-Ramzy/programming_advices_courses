8068057c6af58956cd2c46617627c58deb5e7e20
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-07-12 01:54:01 +0300

    upload file

--- course_3/while_loop.cpp
+++ course_3/while_loop.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int read_int_number_in_range(int from, int to)
+{
+	int number;
+
+	cout << "Please a number between " << from << " and " << to << endl;
+	cin >> number;
+
+	while (number < from || number > to)
+	{
+		cout << "Wrong num, Please enter a valid number between "
+			 << from << " and " << to << endl;
+		cin >> number;
+	}
+
+	return (number);
+}
 
 int main(void)
 {
-	
+	int num = read_int_number_in_range(18, 60);
+	cout << "The number is: " << num << endl;
 }
