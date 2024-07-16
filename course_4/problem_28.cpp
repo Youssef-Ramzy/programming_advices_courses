271567510df8a987ccead0b6bc2a6970306bf26b
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-07-16 22:59:48 +0300

    My solve of problem 28

---
+++ course_4/problem_28.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+/**
+ * Problem:
+Write a program to Sum odd numbers from 1 to N.
+Input
+10
+Outputs
+25
+ */
+
+// My solution
+
+int ReadNumber(void)
+{
+	int N;
+
+	cout << "Please enter N: ";
+	cin >> N;
+
+	return N;
+}
+
+void PrintNumber(int N)
+{
+	cout << N << endl;
+}
+
+int OddNumberLoop(int N)
+{
+	int Sum = 0;
+
+	for (int i = 1; i <= N; i++)
+	{
+		if ((i % 2) != 0)
+			Sum += i;
+	}
+
+	return Sum;
+}
+
+int main(void)
+{
+	int N;
+
+	N = ReadNumber();
+	PrintNumber(OddNumberLoop(N));
+}
+
+
+// PRO solution
+
+// ...
