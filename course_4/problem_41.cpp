4b2c1af1d6caabe3a35c75be2c831194fb0cc018
youssef-ramzy <yousseframzyvd@gmail.com>
2024-09-01 12:39:10 +0300

    solve problem 41

--- course_4/problem_41.cpp
+++ course_4/problem_41.cpp
@@ -3,11 +3,91 @@
 using namespace std;
 
 /**
-
+Problem:
+Write a program to read a NumberOfHours and calculates the number of weeks,
+and days included in that number
+Input
+365
+Outputs
+2.17 Weeks
+15.20 Days
  */
 
 // My solution
 
+float ReadPositiveNumber(string Message)
+{
+    float Number;
 
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+float HoursToWeeks(float Hours)
+{
+    return (Hours / 24) / 7.0;
+}
+
+float HoursToDays(float Hours)
+{
+    return (Hours / 24.0);
+}
+
+int main()
+{
+    float Hours = ReadPositiveNumber("Enter the Hours?");
+
+    cout << HoursToWeeks(Hours) << " Weeks" << endl;
+    cout << HoursToDays(Hours) << " Days" << endl;
+
+    return 0;
+}
 
 // PRO solution
+
+float ReadPositiveNumber(string Message)
+{
+    float Number;
+
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+float hoursToDays(float NuberOfHours)
+{
+    return (float)NumberOfHours/24;
+}
+
+float HoursToWeeks(float NumberOfHours)
+{
+    return NumberOfHours/24/7;
+}
+
+float DaysToWeeks(float NumberOfDays)
+{
+    return NumberOfDays/7;
+}
+
+int main()
+{
+    float NumberOfHours = ReadPositiveNumber("Please enter NumberOfHours?");
+    float NumberOfDays = HoursToDays(NuberOfHours);
+    float NumberOfWeeks = DaysToWeeks(NumberOfDays);
+
+    cout << endl;
+    cout << "Total Hours = " << NumbersOfHours << endl;
+    cout << "Total Days = " << NumberOfDays << endl;
+    cout << "Total Weeks = " << HoursToWeeks(NumberOfHours) << endl;
+
+    return 0;
+}
