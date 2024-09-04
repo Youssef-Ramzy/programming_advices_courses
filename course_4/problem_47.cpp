c566435719108b775bb19b96d9f6b6d5b40c12ba
youssef-ramzy <yousseframzyvd@gmail.com>
2024-09-04 17:05:15 +0300

    solve problem 47

--- course_4/problem_47.cpp
+++ course_4/problem_47.cpp
@@ -3,11 +3,46 @@
 using namespace std;
 
 /**
-
+Problem:
+Write a program to read a LoanAmount and Monthly
+Payment and calculate how many months you need to settle the loan.
+Input
+5000
+500
+Outputs
+10 Months
  */
 
 // My solution
 
+int ReadPositiveNumber(string Message)
+{
+    int Number;
 
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number < 0);
+
+    return Number;
+}
+
+int LoanCalc(int LoanAmount, int LoanPayment)
+{
+    return LoanAmount / LoanPayment;
+}
+
+int main(void)
+{
+    int LoanAmount = ReadPositiveNumber("Enter the Loan Amount?");
+    int LoanPayment = ReadPositiveNumber("Enter the Loan Payment?");
+
+    cout << LoanCalc(LoanAmount, LoanPayment) << " Months" << endl;
+
+    return 0;
+}
 
 // PRO solution
+
+// same as mine
