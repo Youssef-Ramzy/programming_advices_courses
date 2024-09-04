1222da8107cc8bceec4a644a73fa3441fc387aea
youssef-ramzy <yousseframzyvd@gmail.com>
2024-09-04 17:02:26 +0300

    solve problem 46

--- course_4/problem_46.cpp
+++ course_4/problem_46.cpp
@@ -3,11 +3,32 @@
 using namespace std;
 
 /**
-
+Problem:
+Write a program to print all letters from A to Z
+Outputs
+A
+B
+C
+.
+.
+.Z
  */
 
 // My solution
 
+void PrintChars(void)
+{
+    for (int i = 65; i <= 90; i++)
+    {
+        cout << (char)i << endl;
+    }
+}
 
+int main()
+{
+    PrintChars();
+}
 
 // PRO solution
+
+// sam as mine
