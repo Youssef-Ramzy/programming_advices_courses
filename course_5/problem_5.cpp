ba5924dfbc2d777b43c017ca722937239c8a88d4
youssef-ramzy <yousseframzyvd@gmail.com>
2024-10-11 17:03:59 +0300

    My anser for the problem num 5

--- course_5/problem_5.cpp
+++ course_5/problem_5.cpp
@@ -3,13 +3,45 @@ using namespace std;
 
 /**
  * Problem # 5/2
- * 
+ * Write a program to read a number and print it in a reversed order.
+Input:
+1234
+Output:
+4
+3
+2
+1
  */
 
 // My solution
 
+int ReadInput(string Message)
+{
+    int num;
 
+    cout << Message;
+    cin >>num;
+
+    return num;
+}
+
+void NumDivider(int Num)
+{
+    do
+    {
+        cout << Num % 10 << endl;
+        Num = Num / 10;
+    } while (Num > 0);
+}
+
+int main()
+{
+    int Num = ReadInput("Enter the numbers: ");
+
+    NumDivider(Num);
+
+    return 0;
+}
 
 // PRO solution
-
 
---
+++ course_5/problem_9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * Problem # 5/2
+ * 
+ */
+
+// My solution
+
+
+
+// PRO solution
+
