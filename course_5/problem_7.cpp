e13486bfff41d74692102fb7aabfc865a2b969c8
youssef-ramzy <yousseframzyvd@gmail.com>
2024-10-11 18:58:01 +0300

    solve problem 7

--- course_5/problem_7.cpp
+++ course_5/problem_7.cpp
@@ -2,13 +2,42 @@
 using namespace std;
 
 /**
- * Problem # 5/2
- * 
+ * Problem # 7/2
+ * Write a program to read a number and print it reversed.
+ * Input:
+ * 1234
+ * Output:
+ * 4321
  */
 
 // My solution
 
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
 
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
 
-// PRO solution
+    return Number;
+}
 
+void PrintReversedNumber(int Number)
+{
+    while (Number > 0)
+    {
+        cout << Number % 10;
+        Number /= 10;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    PrintReversedNumber(ReadPositiveNumber("Please enter the numbers"));
+
+    return 0;
+}
--- course_5/problem_8.cpp
+++ course_5/problem_8.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 /**
  * Problem # 5/2
- * 
+ * Write a program read a digit and a number, then print the digit frequency in that number
+ * Input:
+ * 12232222
+ * Output:
+ * Digit 2 Frequency is 5 Time(s)
  */
 
 // My solution
