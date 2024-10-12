2590e74a5c824887d00dee0116d5bc1b610606d2
youssef-ramzy <yousseframzyvd@gmail.com>
2024-10-12 22:29:51 +0300

    solve problem 10

--- course_5/problem_10.cpp
+++ course_5/problem_10.cpp
@@ -15,7 +15,53 @@ using namespace std;
 
 // My solution
 
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+    
+    return Number;
+}
 
+int CountNumber(int Number)
+{
+    int Count = 0;
+
+    while (Number > 0)
+    {
+        Count++;
+        Number /= 10;
+    }
+
+    return Count;
+}
+
+void PrintDigits(int Number)
+{
+    int Count = CountNumber(Number), Divisor = 1;
+
+    for (int i = 0; i < Count; i++)
+    {
+        Divisor *= 10;
+    }
+
+    while (Divisor > 0)
+    {
+        cout << (Number / Divisor) % 10 << "\n";
+        Divisor /= 10;
+    }
+}
+
+int main()
+{
+    PrintDigits(ReadPositiveNumber("Please enter the Numbers"));
+
+    return 0;
+}
 
 // PRO solution
 
