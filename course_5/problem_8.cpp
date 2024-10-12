fb22a1ecebe8ff50914302203f74aa6cb18a4c88
youssef-ramzy <yousseframzyvd@gmail.com>
2024-10-12 18:52:34 +0300

    solve problem 8

--- course_5/problem_8.cpp
+++ course_5/problem_8.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 /**
  * Problem # 5/2
- * Write a program read a digit and a number, then print the digit frequency in that number
+ * Write a program that read a digit and a number, then print the digit frequency in that number
  * Input:
  * 12232222
  * Output:
@@ -12,7 +12,50 @@ using namespace std;
 
 // My solution
 
+int ReadePositiveNumber(string Message)
+{
+    int Number = 0;
 
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while(Number <= 0);
 
-// PRO solution
+    return Number;
+}
 
+int NumberFrequency(int Numbers, int Num)
+{
+    int Count = 0;
+
+    while(Numbers > 0)
+    {
+        if (Num == Numbers % 10)
+            Count++;
+        Numbers /= 10;
+    }
+
+    return Count;
+}
+
+void CheckStatus(int Number)
+{
+    int Status = 0, Numbers = Number;
+
+    while (Number > 0)
+    {
+        Status = NumberFrequency(Numbers, Number % 10);
+        if (Status > 0)
+            cout << "Digit " << Number % 10 << " Frequency is " << Status - 1 << " Time(s)" << endl;
+            break;
+        Number /= 10;
+    }
+}
+
+int main()
+{
+    CheckStatus(ReadePositiveNumber("Please enter the Numbers"));
+
+    return 0;
+}
--- course_5/problem_9.cpp
+++ course_5/problem_9.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 /**
  * Problem # 5/2
- * 
+ * Write a program read a number, and then print all digit frequencies in that number
+ * Input
+ * 1223222
+ * Output:
+ * Digit 1 Frequency is 1 Time(s).
+ * Digit 2 Frequency is 5 Time(s).
+ * Digit 3 Frequency is 1 Time(S).
  */
 
 // My solution
