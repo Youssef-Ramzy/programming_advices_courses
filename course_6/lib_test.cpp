b1be096c994709c320560acb410dcfec14ff0033
youssef ramzy <159187304+Rywkse@users.noreply.github.com>
2025-06-18 18:22:52 +0000

    modifie the app

--- course_6/lib_test.cpp
+++ course_6/lib_test.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include "header_files/math_lib.h"
 using namespace std;
+using namespace math_lib;
 
 int main()
 {
-    cout << "1 + 2 = " << math_lib::Sum2Numbers(1, 2) << endl;
-    cout << "3 * 4 = " << math_lib::Multiply2Numbers(3, 4) << endl;
-    cout << "5 - 2 = " << math_lib::Subtract2Numbers(5, 2) << endl;
-    cout << "8 / 2 = " << math_lib::Divide2Numbers(8, 2) << endl;
+    cout << "1 + 2 = " << Sum2Numbers(1, 2) << endl;
+    cout << "3 * 4 = " << Multiply2Numbers(3, 4) << endl;
+    cout << "5 - 2 = " << Subtract2Numbers(5, 2) << endl;
+    cout << "8 / 2 = " << Divide2Numbers(8, 2) << endl;
 
     return 0;
 }
